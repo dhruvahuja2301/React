e5cd6b6ae5eb6570806421ee736e47fa3b388dbd
Dhruv Ahuja <dhruvahuja2301@gmail.com>
2020-06-29 13:02:49 +0530

    initial commit

---
+++ public/precache-manifest.45e829d2530e0cd233f9f4cc3bd14947.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21967f887589365c816b8a2aa34fb04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8e156131eafe769d3ba",
+    "url": "/static/css/2.1bd367ea.chunk.css"
+  },
+  {
+    "revision": "af6a7c3a6b4c2f58ccd1",
+    "url": "/static/css/main.94232a70.chunk.css"
+  },
+  {
+    "revision": "b8e156131eafe769d3ba",
+    "url": "/static/js/2.28f3e358.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.28f3e358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6a7c3a6b4c2f58ccd1",
+    "url": "/static/js/main.c7490d76.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
